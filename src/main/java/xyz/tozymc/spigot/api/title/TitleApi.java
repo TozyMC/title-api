83d5ee681308d988b82e1d9f427e84c363dafbfa
Nguyễn Thanh Tân <tozymc@gmail.com>
2021-07-22 09:44:18 +0700

    Add send title with time unit

--- src/main/java/xyz/tozymc/spigot/api/title/TitleApi.java
+++ src/main/java/xyz/tozymc/spigot/api/title/TitleApi.java
@@ -2,6 +2,8 @@ package xyz.tozymc.spigot.api.title;
 
 import static xyz.tozymc.util.Preconditions.checkNotNull;
 
+import java.util.concurrent.TimeUnit;
+
 import org.bukkit.entity.Player;
 import org.jetbrains.annotations.NotNull;
 import xyz.tozymc.minecraft.MinecraftVersion;
@@ -9,6 +11,7 @@ import xyz.tozymc.spigot.api.title.backend.actionbar.ActionbarApi;
 import xyz.tozymc.spigot.api.title.backend.title.BackendTitleApi;
 import xyz.tozymc.spigot.api.title.backend.title.BukkitTitleApi;
 import xyz.tozymc.spigot.api.title.backend.title.LegacyTitleApi;
+import xyz.tozymc.spigot.api.title.util.Ticks;
 
 @SuppressWarnings("ResultOfMethodCallIgnored")
 public final class TitleApi {
@@ -31,21 +34,37 @@ public final class TitleApi {
       int stay, int fadeOut) {
     checkNotNull(player, "Player cannot be null");
 
-    getInstance().titleApi.sendTitle(player, title, subtitle, fadeIn, stay, fadeOut);
+    getInstance().sendTitle0(player, title, subtitle, fadeIn, stay, fadeOut, null);
+  }
+
+  public static void sendTitle(@NotNull Player player, String title, String subtitle, int fadeIn,
+      int stay, int fadeOut, TimeUnit timeUnit) {
+    checkNotNull(player, "Player cannot be null");
+
+    getInstance().sendTitle0(player, title, subtitle, fadeIn, stay, fadeOut, timeUnit);
   }
 
   public static void sendTitle(@NotNull Player player, @NotNull Title title) {
     checkNotNull(player, "Player cannot be null");
     checkNotNull(title, "Title cannot be null");
 
-    sendTitle(player, title.getTitle(), title.getSubtitle(), title.getFadeIn(), title.getStay(),
-        title.getFadeOut());
+    getInstance().sendTitle0(player, title.getTitle(), title.getSubtitle(), title.getFadeIn(),
+        title.getStay(), title.getFadeOut(), title.getTimeUnit());
   }
 
   public static void sendActionbar(@NotNull Player player, String message) {
     checkNotNull(player, "Player cannot be null");
 
     getInstance().actionbarApi.sendActionbar(player, message);
+  }
+
+  private void sendTitle0(Player player, String title, String subtitle, int fadeIn, int stay,
+      int fadeOut, TimeUnit timeUnit) {
+    fadeIn = Ticks.convertToTick(fadeIn, timeUnit);
+    stay = Ticks.convertToTick(stay, timeUnit);
+    fadeOut = Ticks.convertToTick(fadeOut, timeUnit);
+
+    titleApi.sendTitle(player, title, subtitle, fadeIn, stay, fadeOut);
   }
 
   private static final class TitleApiHelper {
