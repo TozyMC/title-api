6aa15aae71378a90c88c63927249988abd47906b
Nguyễn Thanh Tân <tozymc@gmail.com>
2021-07-23 08:58:46 +0700

    Fix wrong calculate tick

--- src/main/java/xyz/tozymc/spigot/api/title/util/Ticks.java
+++ src/main/java/xyz/tozymc/spigot/api/title/util/Ticks.java
@@ -1,16 +1,19 @@
 package xyz.tozymc.spigot.api.title.util;
 
+import org.jetbrains.annotations.Contract;
+
 import java.util.concurrent.TimeUnit;
 
 public final class Ticks {
 
   private Ticks() {}
 
+  @Contract("_, null -> param1")
   public static int convertToTick(int duration, TimeUnit timeUnit) {
     if (timeUnit == null) {
       return duration;
     }
     long seconds = timeUnit.toSeconds(duration);
-    return Math.toIntExact(seconds / 20);
+    return Math.toIntExact(seconds * 20);
   }
 }
