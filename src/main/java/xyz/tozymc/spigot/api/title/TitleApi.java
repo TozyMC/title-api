431426cb5d9a2755fe2e272e1d45b04944285053
Nguyễn Thanh Tân <tozymc@gmail.com>
2021-07-23 09:12:16 +0700

    Add TitleActionbarApi implement when initial api

--- src/main/java/xyz/tozymc/spigot/api/title/TitleApi.java
+++ src/main/java/xyz/tozymc/spigot/api/title/TitleApi.java
@@ -2,28 +2,33 @@ package xyz.tozymc.spigot.api.title;
 
 import static xyz.tozymc.util.Preconditions.checkNotNull;
 
-import java.util.concurrent.TimeUnit;
-
 import org.bukkit.entity.Player;
 import org.jetbrains.annotations.NotNull;
 import xyz.tozymc.minecraft.MinecraftVersion;
-import xyz.tozymc.spigot.api.title.backend.actionbar.ActionbarApi;
+import xyz.tozymc.spigot.api.title.backend.actionbar.NmsTitleActionbarApi;
+import xyz.tozymc.spigot.api.title.backend.actionbar.SpigotTitleActionbarApi;
+import xyz.tozymc.spigot.api.title.backend.actionbar.TitleActionbarApi;
 import xyz.tozymc.spigot.api.title.backend.title.BackendTitleApi;
 import xyz.tozymc.spigot.api.title.backend.title.BukkitTitleApi;
 import xyz.tozymc.spigot.api.title.backend.title.LegacyTitleApi;
 import xyz.tozymc.spigot.api.title.util.Ticks;
 
+import java.util.concurrent.TimeUnit;
+
 @SuppressWarnings("ResultOfMethodCallIgnored")
 public final class TitleApi {
 
   private final BackendTitleApi titleApi;
-  private final ActionbarApi actionbarApi;
+  private final TitleActionbarApi titleActionbarApi;
 
   private TitleApi() {
-    this.titleApi = MinecraftVersion.getVersion().isOlderThan(MinecraftVersion.v1_11_R1)
+    MinecraftVersion version = MinecraftVersion.getVersion();
+    this.titleApi = version.isOlderThan(MinecraftVersion.v1_11_R1)
         ? new LegacyTitleApi()
         : new BukkitTitleApi();
-    this.actionbarApi = new ActionbarApi();
+    this.titleActionbarApi = version.isOlderThan(MinecraftVersion.v1_9_R1)
+        ? new SpigotTitleActionbarApi()
+        : new NmsTitleActionbarApi();
   }
 
   public static TitleApi getInstance() {
@@ -55,7 +60,7 @@ public final class TitleApi {
   public static void sendActionbar(@NotNull Player player, String message) {
     checkNotNull(player, "Player cannot be null");
 
-    getInstance().actionbarApi.sendActionbar(player, message);
+    getInstance().titleActionbarApi.sendActionbar(player, message);
   }
 
   private void sendTitle0(Player player, String title, String subtitle, int fadeIn, int stay,
